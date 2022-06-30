ac59a69a0ca930968bcb54c188fa6c2c8f887144
Juan Carlos Bindez <79322362+JuanBindez@users.noreply.github.com>
2022-06-30 10:12:20 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -13,16 +13,13 @@ import os
 
 try:
 
-    def cal():
-        print(Color.AMARELO + "calculadora Python!")
-
     def divide():
         try:
             d1 = float(input(Color.AMARELO + "digite o numero para ser  dividido:\n"))
             d2 = float(input("digite o outro numero:\n" + Color.RESET))
             os.system("clear")
             time.sleep(1)
-            cal()
+            credits_author()
             print(Color.VERDE + "o resultado é =", d1 / d2)
         except ZeroDivisionError:
             print(Color.VERMELHO + "erro de divisão por 0" + Color.RESET)
@@ -36,7 +33,7 @@ try:
         m2 = float(input("digite o outro numero:\n" + Color.RESET))
         os.system("clear")
         time.sleep(1)
-        cal()
+        credits_author()
         print(Color.VERDE + "o resultado é =", m1 * m2)
 
 
@@ -45,7 +42,7 @@ try:
         s2 = float(input("digite o outro numero:\n" + Color.RESET))
         os.system("clear")
         time.sleep(1)
-        cal()
+        credits_author()
         print(Color.VERDE + "o resultado é =", s1 - s2)
 
 
@@ -54,36 +51,47 @@ try:
         a2 = float(input("digite o outro numero:\n" + Color.RESET))
         os.system("clear")
         time.sleep(1)
-        cal()
+        credits_author()
         print(Color.VERDE + "o resultado é =", a1 + a2)
 
+    def credits_author():
+        print(
+                            '''
+                            Author: www.github.com/JuanBindez
+                            Description:
+                            Python Version: 3.10
+                            year: 2022
+                            Local: Brazil
+                            '''
+        )
+
     os.system("clear")
-    cal()
-    print(Color.CYANO + "para dividir digite 1")
-    print("para multiplicar digite 2")
-    print("para subtrair digite 3")
-    print("para adição digite 4" + Color.RESET)
+    credits_author()
+    print(Color.CYANO + "[1]  dividir digite ")
+    print("[2]  multiplicar digite ")
+    print("[3]  subtrair digite 3")
+    print("[4]  adição digite 4" + Color.RESET)
 
-    y = int(input(":"))
+    numero = int(input(">"))
     os.system("clear")
 
-    if y == 1:
-        divide()
+    match numero:
 
-    if y == 2:
-        multi()
-
-
-    if y == 3:
-        sub()
-
-
-    if y == 4:
-        adiciona()
+        case 1:
+            divide()
         
+        case 2:
+            multi()
+
+        case 3:
+            sub()
+
+        case 4:
+            adiciona()
+
+
 except KeyboardInterrupt:
     os.system("clear")
     print(Color.VERDE + "obrigado por usar a Calculadora Python!")
-
 
 
